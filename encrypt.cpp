265414b9a4cfcb019fa65028f5748a50fa08f9fd
Mod233 <ccccsober@gmail.com>
2018-03-30 16:20:33 +0800

    ver1.1

--- encrypt.cpp
+++ encrypt.cpp
@@ -5,30 +5,37 @@
 #include <map>
 #include <cstring>
 using namespace std;
+char password[20];
+char plain[100];
+int encrypttext[100];
+void _init(){
+    memset(password,0,sizeof(password));
+    memset(plain,0,sizeof(plain));
+    memset(encrypttext,0,sizeof(encrypttext));
+}
 int main(int argc,const char*argv[]){
-    char password[20];
-    char plain[100];
-    int encrypttext[100];
-    cout<<"Please input the Code:"<<endl;
+    _init();
+    printf("Please input the Code:\n");
     cin>>password;
     printf("Input the model: 0(encrypt) 1(decrypt)\n");
     int t;
     while(scanf("%d",&t)!=EOF){
         printf("Please input your test:\n");
+        memset(plain,0,sizeof(plain));
         if(!t){
             scanf("%s",plain);
-            for(int i=0;i<strlen(plain);i++)
+            for(int i=0;i<16;i++)
                 plain[i]=plain[i]^password[i%strlen(password)];
             printf("According your password,the encrypttext is\n");
-            for(int i=0;i<strlen(plain);i++)
+            for(int i=0;i<16;i++){
                 printf("%d ",plain[i]-'a'+97);
-            printf("-%d\n",strlen(plain));
+            }
+            printf("\n");
             printf("Input the model: 0(encrypt) 1(decrypt)\n");
         }
         else{
             int len;
-            scanf("%d",&len);
-            for(int i=0;i<len;i++){
+            for(int i=0;i<16;i++){
                 scanf("%d",&encrypttext[i]);
                 plain[i]=encrypttext[i];
                 plain[i]=plain[i]^password[i%strlen(password)];
